d7067daabb90f3db07340e772c86b91f45370f0a
Thomas Marble <tmarble3@gmail.com>
2022-03-26 11:15:19 -0600

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] videoGames = { "Halo", "Cod", "Madden" };
+
+            List<string> videoGamesList = new List<string>(videoGames);
+
+            var name = from a in videoGames
+                       orderby a.Length ascending
+                       select a;
+            foreach (var game in name)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
